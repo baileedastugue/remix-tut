01e4a02bc659fecd62808f468f3201247069443c
Bailee Dastugue <bailee.dast@gmail.com>
2024-03-29 17:20:10 -0500

    syncing search with url params

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
-import type { LinksFunction } from '@remix-run/node';
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
+import { useEffect, useState } from 'react';
 import {
   Form,
   Links,
@@ -18,9 +19,11 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: appStylesHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
-  return json({ contacts });
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
 };
 
 export const action = async () => {
@@ -29,8 +32,14 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+
+  const [query, setQuery] = useState(q || '');
+
+  useEffect(() => {
+    setQuery(q || '');
+  }, [q]);
 
   return (
     <html lang='en'>
@@ -48,6 +57,8 @@ export default function App() {
               <input
                 id='q'
                 aria-label='Search contacts'
+                value={query || ''}
+                onChange={(e) => setQuery(e.currentTarget.value)}
                 placeholder='Search'
                 type='search'
                 name='q'
