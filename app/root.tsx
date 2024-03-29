e1e05f39e91206d7afa7397b517675eb45e6e62d
Bailee Dastugue <bailee.dast@gmail.com>
2024-03-29 16:55:56 -0500

    overlay when loading

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,6 @@ import type { LinksFunction } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
 import {
   Form,
-  Link,
   Links,
   Meta,
   NavLink,
@@ -10,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from '@remix-run/react';
 import appStylesHref from './app.css?url';
 import { createEmptyContact, getContacts } from './data';
@@ -30,6 +30,8 @@ export const action = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+
   return (
     <html lang='en'>
       <head>
@@ -86,7 +88,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id='detail'>
+        <div
+          className={navigation.state === 'loading' ? 'loading' : ''}
+          id='detail'
+        >
           <Outlet />
         </div>
         <ScrollRestoration />
